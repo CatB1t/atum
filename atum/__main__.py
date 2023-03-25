3f6e124a8fca89504b619fed650961b4922b2d4f
CatB1t <skytedits@gmail.com>
2023-03-25 09:14:00 +0200

    add basic curses screen instead of command line arguments interface.

--- atum/__main__.py
+++ atum/__main__.py
@@ -1,18 +1,69 @@
+import curses
+from collections import namedtuple
 from atum.tracker import workclock
-import argparse
+from curses import wrapper
+
+CmdListEntry = namedtuple("CmdListEntry", ["name", "fn"])
+
+command_list = [CmdListEntry("Clock In", workclock.clock_in), 
+                CmdListEntry("Reset Clock", workclock.reset_clock)]
+
+def _main(stdscr: curses.window):
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    GREEN_AND_BLACK = curses.color_pair(1)
+    # Show status
+    status_window = stdscr.subwin(5, 100, 0, 0)
+    status_window.box()
+    status_window.addstr(0, 2, "Status")
+    status_pad = stdscr.subpad(1, 97, 2, 2)
+    if workclock.is_clocked_in:
+        status_color = curses.COLOR_GREEN
+    else:
+        status_color = curses.COLOR_RED
+    curses.init_pair(2, status_color, curses.COLOR_BLACK)
+    status_pad.addstr(0, 0, workclock.status, curses.color_pair(2))
+    status_pad.refresh()
+
+    # Show Commands 
+    commands_window = stdscr.subwin(5, 20, 5, 0) 
+    cursor_pos = 0
+
+    commands_window.addch(cursor_pos, 0, '>', GREEN_AND_BLACK)
+    for i, item in enumerate(command_list):
+        commands_window.addstr(i, 2, item.name)
+
+    stdscr.nodelay(True)
+    while True:
+        status_pad.erase()
+        if workclock.is_clocked_in:
+            status_color = curses.COLOR_GREEN
+        else:
+            status_color = curses.COLOR_RED
+        curses.init_pair(2, status_color, curses.COLOR_BLACK)
+        status_pad.addstr(0, 0, workclock.status, curses.color_pair(2))
+        status_pad.refresh()
+
+        try:
+            usr_input = stdscr.getkey().lower()
+        except Exception as e:
+            pass
+        else:
+            if usr_input.lower() == 'q':
+                raise SystemExit
+            elif usr_input == "\n": # Enter
+                command_list[cursor_pos].fn()
+            elif usr_input == 'w' or usr_input == 'key_up':
+                commands_window.addch(cursor_pos, 0, ' ')
+                if cursor_pos > 0:
+                    cursor_pos -= 1
+            elif usr_input == 's' or usr_input == 'key_down':
+                commands_window.addch(cursor_pos, 0, ' ')
+                if cursor_pos < len(command_list) - 1:
+                    cursor_pos += 1
+            
+            commands_window.addch(cursor_pos, 0, '>', GREEN_AND_BLACK)
+            commands_window.refresh()
 
 def main():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(
-        title="Work clock",
-    )
-    clock_in_parser = subparsers.add_parser("clin", help="clock in work")
-    clock_in_parser.set_defaults(func=workclock.clock_in)
-    reset_clock_parser = subparsers.add_parser("rclock", help="reset work clock")
-    reset_clock_parser.set_defaults(func=workclock.reset_clock)
-    status_clock_parser = subparsers.add_parser("status", help="show status of work clock")
-    def print_status():
-        print(workclock.status)
-    status_clock_parser.set_defaults(func=print_status)
-    new_args = parser.parse_args()
-    new_args.func()
+    wrapper(_main)
