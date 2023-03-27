65bb70268bffcf9d8d4abfde380dd23e01a7e753
CatB1t <skytedits@gmail.com>
2023-03-27 08:25:47 +0200

    add tasks tracker, update main interface, remove redundant code for displaying status, update status to show task.

--- atum/__main__.py
+++ atum/__main__.py
@@ -1,5 +1,6 @@
 import curses
 from atum.tracker import workclock
+from atum.tasks import task_tracker
 from .widgets import CommandList, CommandListEntry, MainInputBox
 from curses import wrapper
 
@@ -8,17 +9,10 @@ def _main(stdscr: curses.window):
     curses.curs_set(0)
 
     # Show status
-    status_window = stdscr.subwin(5, 100, 0, 0)
+    status_window = stdscr.subwin(7, 100, 0, 0)
     status_window.box()
     status_window.addstr(0, 2, "Status")
-    status_pad = stdscr.subpad(1, 97, 2, 2)
-    if workclock.is_clocked_in:
-        status_color = curses.COLOR_GREEN
-    else:
-        status_color = curses.COLOR_RED
-    curses.init_pair(2, status_color, curses.COLOR_BLACK)
-    status_pad.addstr(0, 0, workclock.status, curses.color_pair(2))
-    status_pad.refresh()
+    status_pad = stdscr.subpad(3, 97, 2, 2)
 
     # Show Commands
     input_box = MainInputBox()
@@ -31,13 +25,20 @@ def _main(stdscr: curses.window):
         usr_input = input_box.get_input("8:00")
         workclock.clock_in(usr_input)
 
+    def start_task_input():
+        usr_input = input_box.get_input("")
+        task_tracker.start_task(usr_input)
+
     command_list = [
-        CommandListEntry("Clock In", clock_in_with_time),
+        CommandListEntry("Start Task", start_task_input),
+        CommandListEntry("End Task", task_tracker.end_task),
+        # CommandListEntry("Show Tasks", None),
         CommandListEntry("Take a break", take_break),
+        CommandListEntry("Clock In", clock_in_with_time),
         CommandListEntry("Reset Clock", workclock.reset_clock),
     ]
 
-    main_commands = CommandList(stdscr, 5, 0, command_list)
+    main_commands = CommandList(stdscr, 7, 0, command_list)
     stdscr.nodelay(True)
     while True:
         status_pad.erase()
@@ -47,6 +48,18 @@ def _main(stdscr: curses.window):
             status_color = curses.COLOR_RED
         curses.init_pair(2, status_color, curses.COLOR_BLACK)
         status_pad.addstr(0, 0, workclock.status, curses.color_pair(2))
+        status_pad.addstr(
+            1,
+            0,
+            f"Current task: {task_tracker.current_task_name}",
+            curses.color_pair(2),
+        )
+        status_pad.addstr(
+            2,
+            0,
+            f"Been working on task for: {task_tracker.time_on_task}",
+            curses.color_pair(2),
+        )
         status_pad.refresh()
 
         main_commands.show()
---
+++ atum/tasks.py
@@ -0,0 +1,87 @@
+import sys
+import os
+import json
+
+from datetime import datetime, timedelta
+from collections import namedtuple
+
+TaskRecord = namedtuple("TaskRecord", ["start_time", "time"])
+
+
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        if isinstance(obj, timedelta):
+            return str(obj).partition(".")[0]
+        return json.JSONEncoder.default(self, obj)
+
+
+class TaskTracker:
+    def __init__(self, tasks_file="tasks.json"):
+        self.tasks_file_name = tasks_file
+        self.absolute_tasks_file_path = os.path.join(
+            sys.modules["atum"].__path__[0], self.tasks_file_name
+        )
+        self.current_task_start_time = None
+        self.current_task = None
+        self.current_task_name = ""
+        self.tasks = {}
+        if os.path.exists(self.absolute_tasks_file_path):
+            with open(self.absolute_tasks_file_path) as json_file:
+                data_dict = json.load(json_file)
+                for key, value in data_dict["tasks"].items():
+                    self.tasks[key] = TaskRecord(
+                        datetime.fromisoformat(value[0]), value[1]
+                    )
+                self.current_task_name = data_dict["current_task_name"]
+                if self.current_task_name is not None:
+                    self.current_task = self.tasks[self.current_task_name]
+        else:
+            self._write_task_records()
+
+    def _write_task_records(self):
+        with open(self.absolute_tasks_file_path, "w") as json_file:
+            json.dump(
+                {"current_task_name": self.current_task_name, "tasks": self.tasks},
+                json_file,
+                cls=DatetimeEncoder,
+            )
+
+    @property
+    def time_on_task(self):
+        if self.current_task is None:
+            return -1
+
+        time_delta = datetime.now() - self.current_task.start_time
+        return str(time_delta).partition(".")[0]
+
+    def start_task(self, name, try_override=False):
+        if self.current_task is not None:
+            return
+
+        self.current_task = TaskRecord(datetime.now(), None)
+        self.current_task_name = name
+        self.tasks[name] = self.current_task
+        self._write_task_records()
+
+    def end_task(self):
+        if self.current_task is None:
+            return
+
+        start_time = self.current_task.start_time
+        name = self.current_task_name
+        self.current_task = TaskRecord(start_time, datetime.now() - start_time)
+        self.tasks[self.current_task_name] = self.current_task
+        self.current_task = None
+        self.current_task_name = None
+        self._write_task_records()
+
+    def show_tasks(self):  # TODO remove
+        for key, value in self.tasks.items():
+            print(
+                f"Name: {key}, Start time: {value.start_time.strftime('%H:%M:%S')}, Time taken: {value.time}"
+            )
+
+
+task_tracker = TaskTracker()
