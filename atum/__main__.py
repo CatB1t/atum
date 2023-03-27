bc6070aa8c867fbe5ace6720ae73698d42435cf5
CatB1t <skytedits@gmail.com>
2023-03-27 05:51:54 +0200

    Refactor list to a CommandList class, fix linting.

--- atum/__main__.py
+++ atum/__main__.py
@@ -1,12 +1,8 @@
 import curses
-from collections import namedtuple
 from atum.tracker import workclock
+from .widgets import CommandList, CommandListEntry
 from curses import wrapper
 
-CmdListEntry = namedtuple("CmdListEntry", ["name", "fn"])
-
-command_list = [CmdListEntry("Clock In", workclock.clock_in), 
-                CmdListEntry("Reset Clock", workclock.reset_clock)]
 
 def _main(stdscr: curses.window):
     curses.curs_set(0)
@@ -25,14 +21,13 @@ def _main(stdscr: curses.window):
     status_pad.addstr(0, 0, workclock.status, curses.color_pair(2))
     status_pad.refresh()
 
-    # Show Commands 
-    commands_window = stdscr.subwin(5, 20, 5, 0) 
-    cursor_pos = 0
+    # Show Commands
+    command_list = [
+        CommandListEntry("Clock In", workclock.clock_in),
+        CommandListEntry("Reset Clock", workclock.reset_clock),
+    ]
 
-    commands_window.addch(cursor_pos, 0, '>', GREEN_AND_BLACK)
-    for i, item in enumerate(command_list):
-        commands_window.addstr(i, 2, item.name)
-
+    main_commands = CommandList(stdscr, 5, 0, command_list)
     stdscr.nodelay(True)
     while True:
         status_pad.erase()
@@ -44,26 +39,17 @@ def _main(stdscr: curses.window):
         status_pad.addstr(0, 0, workclock.status, curses.color_pair(2))
         status_pad.refresh()
 
+        main_commands.show()
         try:
             usr_input = stdscr.getkey().lower()
         except Exception as e:
             pass
         else:
-            if usr_input.lower() == 'q':
+            if usr_input.lower() == "q":
                 raise SystemExit
-            elif usr_input == "\n": # Enter
-                command_list[cursor_pos].fn()
-            elif usr_input == 'w' or usr_input == 'key_up':
-                commands_window.addch(cursor_pos, 0, ' ')
-                if cursor_pos > 0:
-                    cursor_pos -= 1
-            elif usr_input == 's' or usr_input == 'key_down':
-                commands_window.addch(cursor_pos, 0, ' ')
-                if cursor_pos < len(command_list) - 1:
-                    cursor_pos += 1
-            
-            commands_window.addch(cursor_pos, 0, '>', GREEN_AND_BLACK)
-            commands_window.refresh()
+            else:
+                main_commands.handle(usr_input)
+
 
 def main():
     wrapper(_main)
